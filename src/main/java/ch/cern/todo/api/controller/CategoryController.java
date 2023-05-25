7490ddd7c67506413694f45facc70a8ebff96467
Levi <gyorgydeak.levente@gmail.com>
2023-05-25 22:26:50 +0200

    category get all

--- src/main/java/ch/cern/todo/api/controller/CategoryController.java
+++ src/main/java/ch/cern/todo/api/controller/CategoryController.java
@@ -1,9 +1,22 @@
-package ch.cern.todo.api;
+package ch.cern.todo.api.controller;
 
+import ch.cern.todo.api.dto.CategoryDTO;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("category")
 public class CategoryController {
+
+
+    @GetMapping
+    Collection<CategoryDTO> getAll(){
+        var response = new ArrayList<CategoryDTO>();
+        response.add(new CategoryDTO("category1","description1"));
+        return response;
+    }
 }
--- src/main/java/ch/cern/todo/api/dto/CategoryDTO.java
+++ src/main/java/ch/cern/todo/api/dto/CategoryDTO.java
@@ -1,2 +1,11 @@
-package ch.cern.todo.api.dto;public class CategoryDTO {
+package ch.cern.todo.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class CategoryDTO {
+    private String name;
+    private String description;
 }
