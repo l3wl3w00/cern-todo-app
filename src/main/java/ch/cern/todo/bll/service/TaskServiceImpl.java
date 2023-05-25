4138ade35844f93ab1dfa6a1d0b394211b3460a0
Levi <gyorgydeak.levente@gmail.com>
2023-05-26 00:39:31 +0200

    implement task service (for real this time)

--- src/main/java/ch/cern/todo/bll/service/TaskServiceImpl.java
+++ src/main/java/ch/cern/todo/bll/service/TaskServiceImpl.java
@@ -1,33 +1,66 @@
 package ch.cern.todo.bll.service;
 
+import ch.cern.todo.api.dto.CategoryDTO;
 import ch.cern.todo.api.dto.TaskDTO;
+import ch.cern.todo.bll.exception.NoSuchEntityException;
 import ch.cern.todo.bll.interfaces.TaskService;
+import ch.cern.todo.dal.entity.CategoryEntity;
+import ch.cern.todo.dal.entity.TaskEntity;
+import ch.cern.todo.dal.repository.CategoryRepository;
+import ch.cern.todo.dal.repository.TaskRepository;
+import ch.cern.todo.mapping.CategoryMapper;
+import ch.cern.todo.mapping.TaskMapper;
+import lombok.RequiredArgsConstructor;
 
 import java.util.Collection;
 
+@RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService {
+    private final TaskRepository taskRepository;
+    private final CategoryRepository categoryRepository;
+    private final TaskMapper taskMapper;
+    private final CategoryMapper categoryMapper;
+
     @Override
     public Collection<TaskDTO> getAll() {
-        return null;
+        return taskMapper.toDTOCollection(taskRepository.findAll());
     }
 
     @Override
     public TaskDTO getById(Long id) {
-        return null;
+        return taskMapper.toDTO(findOrThrow(id));
     }
 
     @Override
     public TaskDTO add(TaskDTO taskDTO) {
-        return null;
+        return addOrUpdate(taskDTO);
     }
 
     @Override
     public TaskDTO update(TaskDTO taskDTO) {
-        return null;
+        return addOrUpdate(taskDTO);
     }
 
     @Override
     public TaskDTO deleteById(Long id) {
-        return null;
+        var entity = findOrThrow(id);
+
+        taskRepository.deleteById(id);
+
+        return taskMapper.toDTO(entity);
+    }
+
+    private TaskDTO addOrUpdate(TaskDTO taskDTO){
+        var mappedEntity = taskMapper.toEntity(taskDTO);
+
+        var savedEntity = taskRepository.save(mappedEntity);
+
+        return taskMapper.toDTO(savedEntity);
+    }
+    private TaskEntity findOrThrow(Long id) {
+        return taskRepository
+                .findById(id)
+                .orElseThrow(() ->
+                        new NoSuchEntityException(TaskEntity.class,id));
     }
 }
