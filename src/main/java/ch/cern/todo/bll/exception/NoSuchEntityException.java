d44b016bd6247e3f57a78ba6ca1ac7cdbefdc4ee
Levi <gyorgydeak.levente@gmail.com>
2023-05-27 14:16:58 +0200

    generic NoSuchEntityException ctor

--- src/main/java/ch/cern/todo/bll/exception/NoSuchEntityException.java
+++ src/main/java/ch/cern/todo/bll/exception/NoSuchEntityException.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class NoSuchEntityException extends RuntimeException {
-    public NoSuchEntityException(Class type, Long id) {
+    public <T> NoSuchEntityException(Class<T> type, Long id) {
         super(String.format("No %s entity exists with id %d", type.getName(), id));
     }
 }
