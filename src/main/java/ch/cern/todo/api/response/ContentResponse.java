c0ef84f23356920005a05bd949d0257b1404a37b
Levi <gyorgydeak.levente@gmail.com>
2023-05-28 15:03:25 +0200

    remove unused config variables

--- src/main/java/ch/cern/todo/api/response/ContentResponse.java
+++
@@ -1,9 +0,0 @@
-package ch.cern.todo.api.response;
-
-import lombok.*;
-import org.springframework.http.HttpStatus;
-
-
-public class ContentResponse<DTO> extends Response<DTO>{
-
-}
--- src/main/java/ch/cern/todo/config/Config.java
+++ src/main/java/ch/cern/todo/config/Config.java
@@ -1,7 +1,6 @@
 package ch.cern.todo.config;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "constraints")
@@ -11,18 +10,4 @@ public class Config {
     private Integer maxTaskDescriptionLength;
     private Integer maxCategoryNameLength;
     private Integer maxCategoryDescriptionLength;
-
-    @Value("${constraints.maxTaskNameLength}")
-    public static final int MAX_TASK_NAME_LENGTH = 0;
-
-    @Value("${constraints.maxTaskDescriptionLength}")
-    public static final int MAX_TASK_DESCRIPTION_LENGTH = 0;
-
-
-    @Value("${constraints.maxCategoryNameLength}")
-    public static final int MAX_CATEGORY_NAME_LENGTH = 0;
-
-
-    @Value("${constraints.maxCategoryDescriptionLength}")
-    public static final int MAX_CATEGORY_DESCRIPTION_LENGTH = 0;
 }
